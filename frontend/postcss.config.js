c3a6eb757da3a497f3aa0ef90abbb40b174376d8
rizqisa <rizqisa1809@students.amikom.ac.id>
2025-05-04 16:30:36 +0700

    config css

---
+++ frontend/postcss.config.js
@@ -0,0 +1,9 @@
+import autoprefixer from "autoprefixer";
+import tailwindConfig from "./tailwind.config";
+
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
